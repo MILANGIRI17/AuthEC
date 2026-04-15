a7d61f922e7518143377189d7a4c0dc804131e4a
milan giri <milan.giri17021999@gmail.com>
2026-04-15 21:13:53 +0545

    Angular setup and user registration form is also created

---
+++ frontend/AuthECClient/src/app/user/registration/registration.ts
@@ -0,0 +1,25 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-registration',
+  imports: [ReactiveFormsModule],
+  templateUrl: './registration.html',
+  styles: ``,
+})
+export class Registration {
+
+  formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    fullName : [''],
+    email : [''],
+    password : [''],
+    confirmPassword : [''],
+  })
+
+  onSubmit()
+  {
+    console.log(this.form.value);
+  }
+}
